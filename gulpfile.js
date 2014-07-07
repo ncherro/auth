8608bbec573814d5b99acefb04bf8455838bbadc
Nick Herro <ncherro@gmail.com>
2014-07-07 11:31:45 -0400

    working on gulpfile (currently broken)

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
     concat = require('gulp-concat'),
-    rev = required('gulp-rev');
+    rev = required('gulp-rev'),
+    coffee = require('gulp-coffee');
 
 gulp.task('less', function() {
   return gulp.src('frontend/src/css/app.less')
@@ -15,12 +16,17 @@ gulp.task('watch', ['less'], function() {
 
 gulp.task('watch', ['less', 'scripts'], function() {
   gulp.watch('frontend/src/css/**/*.less', ['less']);
-  gulp.watch('frontend/src/js/**/*.js', ['scripts']);
+  gulp.watch('frontend/src/js/**/*.coffee', ['scripts']);
 });
 
+gulp.task('coffee', function() {
+  gulp.src('frontend/src/*.coffee')
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(gulp.dest('frontend/dist/'));
+});
 
 gulp.task('rev', ['less', 'scripts'], function() {
-  return gulp.src(['dist/**/*.css', 'dist/**/*.js'])
+  return gulp.src(['frontend/dist/css/**/*.css', 'frontend/dist/js/**/*.js'])
     .pipe(rev())
     .pipe(gulp.dest('dist'))
     .pipe(rev.manifest())
